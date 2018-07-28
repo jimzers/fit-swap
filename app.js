f15f4faad38f047233466e604e0f5d9669c02732
jimzers <38634441+jimzers@users.noreply.github.com>
2018-07-28 13:11:34 -0700

    connect firebase to app and added require

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var expressValidator = require('express-validator');
+var firebase = require("firebase");
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyDkm1KG9gyzQn70FWalWm19OYzGRxxJWoA",
+    authDomain: "fitswap-f2c33.firebaseapp.com",
+    databaseURL: "https://fitswap-f2c33.firebaseio.com",
+    storageBucket: "fitswap-f2c33.appspot.com",
+};
+firebase.initializeApp(config);
+
 
 var app = express();
+
