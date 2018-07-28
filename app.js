84475cbc2d46810496480e09ab0675587d7eba8d
Marshall Amey <marshall.amey@gmail.com>
2018-07-28 13:19:11 -0700

    add server listener

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var expressValidator = require('express-validator');
 
 var app = express();
+
+
+/** SERVER LISTENER **/
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+   console.log("The FitSwap server has started!");
+});
